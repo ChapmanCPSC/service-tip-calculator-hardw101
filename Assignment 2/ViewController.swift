b254bc88ca0232d9ef0051c704213dbd40de9ede
hardw101 <hardw101@mail.chapman.edu>
2016-04-15 22:04:20 -0700

    Initial Commit

---
+++ Assignment 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment 2
+//
+//  Created by Chris Hardwick on 4/15/16.
+//  Copyright (c) 2016 Chris Hardwick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
